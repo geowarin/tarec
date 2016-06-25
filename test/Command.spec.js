e6cef7ccfa203805fc908e77acdf22688db858bb
geowarin <geowarin@gmail.com>
2016-06-26 01:52:36 +0200

    Add show help test

--- test/Command.spec.js
+++ test/Command.spec.js
@@ -3,6 +3,7 @@
 const expect = require('expect');
 const Commands = require('../lib/commands/Commands');
 const test = require('ava');
+const debug = require('../lib/utils/debug');
 
 test('should call the command', t => {
 
@@ -13,15 +14,19 @@ test('should call the command', t => {
   t.true(hasBeenCalled, 'testCommand should have been called');
 });
 
-// it('should display help', () => {
-//   let captured_text = "";
-//   const commands = new Commands();
-//
-//   var unhook_intercept = captureStdout(txt => captured_text += txt);
-//   commands.addCommand('commandWithHelp', () => {});
-//   commands.addCommandHelp('commandWithHelp', 'help');
-//   commands.runCommand('commandWithHelp', {}, {help: true});
-//   unhook_intercept();
-//
-//   expect(captured_text).toEqual('help');
-// });
+test('should display help', t => {
+
+  const commands = new Commands();
+  
+  debug.capture();
+  commands.addCommandHelp('cmd', 'cmdHelp');
+  commands.showHelp('v0');
+  t.deepEqual(debug.capturedMessages, [
+    'Tarec v0',
+    'Available commands:',
+    '* cmd',
+    'cmdHelp',
+    'Type tarec <command> --help for more information on a specific command'
+  ]);
+  debug.endCapture();
+});

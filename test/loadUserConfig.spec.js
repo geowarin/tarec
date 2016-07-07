64cb0b8962fb9f0acfc7bbf9473e5e0ef2309109
Geoffroy Warin <geowarin@gmail.com>
2016-07-07 11:15:40 +0200

    Add more configuration options for happypack

--- test/loadUserConfig.spec.js
+++ test/loadUserConfig.spec.js
@@ -2,13 +2,18 @@ const expect = require('expect');
 const path = require('path');
 const loadUserConfig = require('../lib/config/loadUserConfig');
 const test = require('ava');
+const os = require('os');
 
 test('config : should parse config', () => {
   process.env['ENV_VAR'] = 'http://localhost:8181';
   const projectDir = path.resolve('fixtures/config/fullConfig.yml');
   const userConfig = loadUserConfig(projectDir);
 
-  expect(userConfig.happypack).toEqual(true);
+  expect(userConfig.happypack).toEqual({
+    enabled: true,
+    cache: true,
+    cpus: 2
+  });
 
   expect(userConfig.aliases).toEqual([
     {expose: 'compo', src: './src/components'},
@@ -30,7 +35,11 @@ test('config : should parse empty config', () => {
   const projectDir = path.resolve('fixtures/config/emptyConfig.yml');
   const userConfig = loadUserConfig(projectDir);
 
-  expect(userConfig.happypack).toEqual(false);
+  expect(userConfig.happypack).toEqual({
+    enabled: false,
+    cache: true,
+    cpus: os.cpus().length
+  });
   expect(userConfig.aliases).toEqual([]);
   expect(userConfig.proxies).toEqual([]);
   expect(userConfig.define).toEqual({});
